84800d91cec2e29bbd94d7143c46bfe843ce6e6b
sai0499 <128142974+sai0499@users.noreply.github.com>
2023-12-05 13:45:41 +0100

    Delete Adaptive Sorting directory

--- Adaptive Sorting/Polygon.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include "Polygon.hpp"
-#include <algorithm>
-
-void Polygon::sortPoints(std::vector<std::pair<double, double>>& points) {
-    // Sort points based on X coordinate
-    std::sort(points.begin(), points.end(), [](const auto& a, const auto& b) {
-        return a.first < b.first;
-    });
-}
